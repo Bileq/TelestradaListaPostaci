caa895dd9efebe87b098ef0952b6bae020360686
Dominik Bil <dominik.bil64@gmail.com>
2021-10-10 11:05:00 +0200

    Handle API connection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import { useState } from 'react';
-import './App.css';
-import Characters from './components/Characters';
-import MainPage from './components/MainPage'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Characters from "./components/Characters";
+import MainPage from "./components/MainPage";
 
 function App() {
-  return (
-    <div className="App">
-      <MainPage />
-      <Characters />   
-    </div>
-  );
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result.results);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="App">
+                <MainPage />
+                <Characters/>
+            </div>
+        );
+    }
 }
 
 export default App;
